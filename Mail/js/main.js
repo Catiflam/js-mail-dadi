31444c80b1575cbed76340a5b0a528183f094b14
Catiflam <gilfrid1@gmail.com>
2023-07-24 16:57:34 +0200

    edit ciclo for

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,8 +1,19 @@
 const mails = ["user@gmail.com", "user@libero.it", "user@hotmail.com", "user@yahoo.com"];
-console.log(mails);
 
 const userMail = prompt("inserisci la tua mail");
 
-// for (let i = 0; i > mails.length; i++) {
-
+// if (userMail < mails.length && !isNaN(userMail)) {
+// 	console.log(mails[userMail]);
 // }
+let found = false;
+for (let i = 0; i < mails.length; i++) {
+	if (userMail == mails[i]) {
+		found = true;
+	}
+}
+
+if (found) {
+	console.log("ti ho trovato");
+} else {
+	console.log("non ti ho trovato");
+}
