57094696dd9bf6acb60ed78fde819fd8079b9e64
Catiflam <gilfrid1@gmail.com>
2023-07-24 16:06:17 +0200

    edit initial

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,1 +1,8 @@
+const mails = ["user@gmail.com", "user@libero.it", "user@hotmail.com", "user@yahoo.com"];
+console.log(mails);
+
 const userMail = prompt("inserisci la tua mail");
+
+// for (let i = 0; i > mails.length; i++) {
+
+// }
